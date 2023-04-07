78e4ed414d3ae44b39e810657378379f8667f80a
Chaitanya Singh Kushwaha <hicsk24@gmail.com>
2023-04-07 22:32:12 +0530

    Update enumeration.py

--- enumeration.py
+++ enumeration.py
@@ -1,3 +1,3 @@
-list1=["Earth","Globe","Planet","Soil","World","Sphere","Orb","Lan"]
+list1=["Earth","Globe","Planet","Soil","World","Sphere","Orb","Land"]
 for x,y in enumerate(list1):
     print(y," is at ",x," position")
